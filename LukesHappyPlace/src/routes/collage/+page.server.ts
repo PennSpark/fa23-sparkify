a831cc01a6594e7a3b6e0068f322305d7b6a44f0
luke-rt <luke.rtong@gmail.com>
2023-11-18 18:06:59 -0500

    large ui changes and fixed access token expired bug

--- LukesHappyPlace/src/routes/collage/+page.server.ts
+++ LukesHappyPlace/src/routes/collage/+page.server.ts
@@ -1,3 +1,5 @@
+import { redirect } from "@sveltejs/kit";
+
 export async function load({ cookies, fetch }) {
     const access_token = cookies.get("access_token");
     // TODO: deal with login + handle access_token expired
@@ -6,6 +8,8 @@ export async function load({ cookies, fetch }) {
 
     const response = await fetch("/api/spotify");
     const data = await response.json();
+
+    if(data.expired) throw redirect(302, "/auth/login");
 
     return await data;
 }
