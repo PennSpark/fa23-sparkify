c2b48621f2ea4a4bfc32508775c1dc556bf32ec5
luke-rt <luke.rtong@gmail.com>
2023-12-28 14:57:38 -0500

    file tree

--- LukesHappyPlace/src/lib/state.ts
+++
@@ -1,8 +0,0 @@
-import { get, readable } from 'svelte/store';
-import { generateRandomString } from './utils';
-
-const state = readable(generateRandomString(16));
-
-export default function getState() {
-	return get(state);
-}
